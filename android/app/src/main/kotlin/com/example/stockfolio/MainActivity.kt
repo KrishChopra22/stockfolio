5947da515fcebd88f1de00c8472b407f90808a7c
Krish Chopra <krishchopra22@gmail.com>
2024-03-04 20:08:03 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/stockfolio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stockfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stock Folio',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('StockFolio'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to StockFolio',
+        ),
+      ),
+    );
+  }
+}
